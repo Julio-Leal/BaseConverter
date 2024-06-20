8256eaa101d031b1fb9c6fa5175100fec4cdaef9
forest522 <91677208+forest522@users.noreply.github.com>
2024-06-20 19:48:37 -0300

    Cria o método converter OCTAL para DECIMAL

--- code/Decimal.java
+++ code/Decimal.java
@@ -1,17 +1,52 @@
+import java.util.List;
+import java.util.ArrayList;
+
 public class Decimal{
-    private int decimal;
+    private int binarioDecimal;
+    private List<Integer> octalDecimal;
 
     public Decimal(){
-        this.decimal = 0;
+        this.binarioDecimal = 0;
+        this.octalDecimal = new ArrayList<Integer> ();
     }
 
     public void converterBinario(String num){
-        
-        if(num.matches("[01]+")){
-            this.decimal = Integer.parseInt(num, 2);
 
-            System.out.println("O número binário "+num+" em decimal equivale a:\n"+this.decimal);
+        if(num.matches("[01]+")){
+            this.binarioDecimal = Integer.parseInt(num, 2);
+
+            System.out.println("O número binário "+num+" em decimal equivale a:\n"+this.binarioDecimal);
         } else
-        System.out.println("O tipo informado não pertence a base binária!");
+            System.out.println("O tipo informado não pertence a base BINÁRIA!");
+
+    }
+
+    public void converterOctal(String num){
+        int i, tempInt;
+        char tempChar;
+
+        if(num.matches("[0-7]+")){
+            octalDecimal.clear();
+
+            for(i=0;i < num.length();i++){
+                tempChar = num.charAt(i);
+                tempInt = Character.getNumericValue(tempChar);
+                octalDecimal.add(tempInt*(int)Math.pow(8.0, num.length() - 1 - i));
+            }
+
+            System.out.println("O número octal "+num+" em decimal é: \n"+this.somarConversaoOctal());
+
+        } else
+            System.out.println("O tipo informado não pertence a base OCTAL");
+    }
+
+    private int somarConversaoOctal(){
+        int soma = 0;
+
+        for(int i : octalDecimal){
+            soma += i;
+        }
+
+        return soma;
     }
 }
