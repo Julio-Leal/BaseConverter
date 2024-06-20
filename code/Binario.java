7b2a2241ee3d3fd112b5f317b2c2331a00526c26
Júlio Leal <cavalcantejulio522@gmail.com>
2024-06-20 16:52:55 +0000

    Upload Binario.java class

---
+++ code/Binario.java
@@ -0,0 +1,40 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Binario implements Base{
+    private List<Integer> binario;
+    
+    public Binario() {
+        this.binario = new ArrayList <Integer> ();
+    }
+
+    public void calcularConversao(String num){
+        int numInteiro, resto;
+        
+        if(num.matches("\\d+")){
+            numInteiro = Integer.parseInt(num); 
+            binario.clear();
+
+            do{
+                resto = numInteiro%2;
+                numInteiro = numInteiro/2;
+
+                binario.add(resto);
+            }while(numInteiro != 0);
+            
+            System.out.println("O número "+num+" equivale a :");
+            this.listarSequencia();
+        }else 
+            System.out.println("O tipo informado não pertence a base decimal!");
+    }
+
+    private void listarSequencia(){
+        int i;
+
+        for(i = binario.size() - 1 ; i >=0 ;i--){
+            System.out.printf("%d", binario.get(i));
+        }
+        System.out.println();
+    }
+}
