f115ba51bd18509bf14413952cc6c4d1d9094a5d
Júlio Leal <cavalcantejulio522@gmail.com>
2024-06-20 20:04:20 +0000

    Adiciona o teste de conversao de binário para decimal

--- code/Teste.java
+++ code/Teste.java
@@ -2,6 +2,10 @@ public class Teste {
     public static void main(String[] args) {
         Binario binario = new Binario();
 
-        binario.calcularConversao("9585757");
+        binario.calcularConversao("1979");
+        
+        Decimal decimal = new Decimal();
+        decimal.converterBinario("11110111011");
+
     }
 }
