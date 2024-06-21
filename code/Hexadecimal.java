463a9a069a4673e0f870d35297957ac11e457737
forest522 <91677208+forest522@users.noreply.github.com>
2024-06-21 20:47:15 -0300

    Upload Hexadecimal.java file

---
+++ code/Hexadecimal.java
@@ -0,0 +1,9 @@
+public class Hexadecimal {
+    
+    public void converterOctal(String num){
+        if(num.matches("[0-7]+")){
+
+        } else  
+            System.out.println("O tipo informado não pertence a base OCTAL");
+    }
+}
