0a8eba2c3d685bb42546bd1f0cb8fc1b77f23fa8
forest522 <91677208+forest522@users.noreply.github.com>
2024-06-22 15:30:21 -0300

    Acrescenta mais métodos de conversão na classe e organiza

--- code/Binario.java
+++ code/Binario.java
@@ -1,42 +1,75 @@
+public class Binario implements Base {
+    private StringBuilder binario;
+    private String resultado;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class Binario implements Base{
-    private List<Integer> binario;
-    
     public Binario() {
-        this.binario = new ArrayList<Integer> ();
+        this.resultado = "";
+        this.binario = new StringBuilder();
     }
 
-    public void calcularConversao(String num){
-        int numInteiro, resto;
-        
-        if(num.matches("\\d+")){
-            numInteiro = Integer.parseInt(num); 
-            binario.clear();
-
-            do{
-
-                resto = numInteiro%2;
-                numInteiro = numInteiro/2;
-                binario.add(resto);
-
-            }while(numInteiro != 0);
-            
-            System.out.println("O número "+num+" equivale a :");
-            this.listarSequencia();
-        }else 
-            System.out.println("O tipo informado não pertence a base decimal!");
+    @Override
+    public String converterHexadecimal(String num) {
+        return null;
     }
 
-    private void listarSequencia(){
+    @Override
+    public String converterDecimal(String num) {
+        if (num.matches("\\d+")) {
+            int numInteiro = Integer.parseInt(num);
+            int resto;
+
+            do {
+
+                resto = numInteiro % 2;
+                numInteiro = numInteiro / 2;
+                binario.append(resto);
+
+            } while (numInteiro != 0);
+
+            this.resultado = binario.reverse().toString(); 
+
+        } else 
+            this.resultado = "O tipo informado não pertence a base decimal!";
+
+        return this.resultado;
+    }
+
+    @Override
+    public String converterOctal(String num) {
         int i;
+        char tempChar;
 
-        for(i = binario.size() - 1 ; i >=0 ;i--){
-            System.out.printf("%d", binario.get(i));
-        }
+        if(num.matches("[0-7]+")){
+            for(i=0; i<num.length();i++){
+                tempChar = num.charAt(i);
+                binario.append(compararValores(tempChar));
+            }
 
-        System.out.println();
+            this.resultado = binario.toString();
+        } else
+            this.resultado = "O timpo informado não pertence a base Octal!";
+        
+        return this.resultado;
     }
+    
+    //submethod from converterOctal()
+    public String compararValores(char valor){
+        String retorno = "";
+        switch(valor){
+            case '0': retorno = "000"; break;
+            case '1': retorno = "001"; break;
+            case '2': retorno = "010"; break;
+            case '3': retorno = "011"; break;
+            case '4': retorno = "100"; break;
+            case '5': retorno = "101"; break;
+            case '6': retorno = "110"; break;
+            case '7': retorno = "111"; break;
+        }
+        return retorno;
+    }
+
+    public String getResultado() {
+        return this.resultado;
+    }
+
 }
