8a5492a037167ce409050be792def2cd6cd0272a
forest522 <91677208+forest522@users.noreply.github.com>
2024-06-22 15:26:12 -0300

    Organiza e aprimora os métodos da classe

--- code/Decimal.java
+++ code/Decimal.java
@@ -1,96 +1,90 @@
 import java.util.List;
 import java.util.ArrayList;
 
-public class Decimal{
+public class Decimal implements Base {
     private int binarioDecimal;
     private List<Integer> listaDecimal;
+    private String resultado;
 
-    public Decimal(){
+    public Decimal() {
         this.binarioDecimal = 0;
-        this.listaDecimal = new ArrayList<Integer> ();
+        this.listaDecimal = new ArrayList<Integer>();
+        this.resultado = "";
     }
-    //Conversão de binário para decimal;
-    public void converterBinario(String num){
 
-        if(num.matches("[01]+")){
+    @Override
+    public String converterDecimal(String num) {
+        return null; 
+    }
+
+    public String converterBinario(String num) {
+        if (num.matches("[01]+")) {
             this.binarioDecimal = Integer.parseInt(num, 2);
 
-            System.out.println("O número binário "+num+" em decimal equivale a:\n"+this.binarioDecimal);
-        } else
-            System.out.println("O tipo informado não pertence a base BINÁRIA!");
-
-    }
-
-    //Conversão de Octal para decimal
-    public void converterOctal(String num){
-        int i, tempInt;
-        char tempChar;
-
-        if(num.matches("[0-7]+")){
-            listaDecimal.clear();
-
-            for(i=0;i < num.length();i++){
-                tempChar = num.charAt(i);
-                tempInt = Character.getNumericValue(tempChar);
-                listaDecimal.add(tempInt*(int)Math.pow(8.0, num.length() - 1 - i));
-            }
-
-            System.out.println("O número octal "+num+" em decimal é: \n"+this.somarConversao());
-
-        } else
-            System.out.println("O tipo informado não pertence a base OCTAL");
-    }
-
-    private int somarConversao(){
-        int soma = 0;
-
-        for(int i : listaDecimal){
-            soma += i;
+            this.resultado = String.valueOf(binarioDecimal);
+        } else {
+            this.resultado = "O tipo informado não pertence a base BINÁRIA!";
         }
 
+        return this.resultado;
+    }
+
+    public String converterOctal(String num) {
+        int tempInt;
+        char tempChar;
+
+        if (num.matches("[0-7]+")) {
+            listaDecimal.clear();
+            for (int i = 0; i < num.length(); i++) {
+                tempChar = num.charAt(i);
+                tempInt = Character.getNumericValue(tempChar);
+                listaDecimal.add(tempInt * (int) Math.pow(8.0, num.length() - 1 - i));
+            }
+            this.resultado = String.valueOf(this.somarConversao());
+        } else {
+            this.resultado = "O tipo informado não pertence a base OCTAL";
+        }
+
+        return this.resultado;
+    }
+
+    public String converterHexadecimal(String num) {
+        int tempInt;
+        char tempChar;
+
+        if (num.matches("[0-9A-Fa-f]+")) {
+            listaDecimal.clear();
+            for (int i = 0; i < num.length(); i++) {
+                tempChar = num.charAt(i);
+                switch (tempChar) {
+                    case 'A': tempInt = 10; break;
+                    case 'B': tempInt = 11; break;
+                    case 'C': tempInt = 12; break;
+                    case 'D': tempInt = 13; break;
+                    case 'E': tempInt = 14; break;
+                    case 'F': tempInt = 15; break;
+                    default: tempInt = Character.getNumericValue(tempChar);
+                }
+                listaDecimal.add(tempInt * (int) Math.pow(16, num.length() - 1 - i));
+            }
+
+            this.resultado = String.valueOf(this.somarConversao());
+        } else {
+            this.resultado = "O tipo informado não pertence a base HEXADECIMAL";
+        }
+
+        return this.resultado;
+    }
+
+    private int somarConversao() {
+        int soma = 0;
+        for (int i : listaDecimal) {
+            soma += i;
+        }
         return soma;
     }
 
-    public void converterHexadecimal(String num){
-        int i, tempInt;
-        char tempChar;
-        
-        if(num.matches("[0-9A-Fa-f]+")){
-            listaDecimal.clear();
-
-            for(i=0;i < num.length();i++){
-                tempChar = num.charAt(i);
-                switch(tempChar){
-                    case 'A':
-                        tempInt = 10;
-                        break;
-                    case 'B':
-                        tempInt = 11;
-                        break;
-                    case 'C':
-                        tempInt = 12;
-                        break;
-                    case 'D':
-                        tempInt = 13;
-                        break;
-                    case 'E':
-                        tempInt = 14;
-                        break;
-                    case 'F':
-                        tempInt = 15;
-                        break;
-                    default: 
-                        tempInt = Character.getNumericValue(tempChar);
-                }
-
-                listaDecimal.add(tempInt*(int)Math.pow(16, num.length() - 1 - i));
-            }
-
-            System.out.println("O número HEXADECIMAL "+num+" em decimal é: \n"+this.somarConversao());
-            
-        } else  
-            System.out.println("O tipo informado não pertence a base HEXADECIMAL");
+    public String getResultado() {
+        return resultado;
     }
-
-    
 }
